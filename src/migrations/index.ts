26afb046c88b866550d9375d338a7399ff81ec39
Michal <michal.irzylowski@gmail.com>
2026-04-04 16:23:18 +0200

    chore: add missing migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20260304_200717 from './20260304_200717';
 import * as migration_20260404_093152 from './20260404_093152';
+import * as migration_20260404_142243 from './20260404_142243';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20260404_093152.up,
     down: migration_20260404_093152.down,
-    name: '20260404_093152'
+    name: '20260404_093152',
+  },
+  {
+    up: migration_20260404_142243.up,
+    down: migration_20260404_142243.down,
+    name: '20260404_142243'
   },
 ];
